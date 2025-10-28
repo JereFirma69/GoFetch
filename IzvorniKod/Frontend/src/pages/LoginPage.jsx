9145fc5e8a7fea808064ae67fc33e6178b65d15b
AnjaCerkez <anja.cerkez@fer.hr>
2025-10-28 12:06:38 +0100

    - Uređivanje izgleda Log In stranice

--- IzvorniKod/Frontend/src/pages/LoginPage.jsx
+++ IzvorniKod/Frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,15 @@
 import {Link} from 'react-router-dom'
+import {FcGoogle} from "react-icons/fc";
+import logoImg from '../assets/logo.png'
 
 function LoginPage(){
     return (
         <div className='authorisation-container'>
-            <h2>GoFetch!</h2>
-            <button className='google-btn'>Continue with Google</button>
+            <div className="gf-title">
+                <h2>GoFetch!</h2>
+               <img className='logo-img' src={logoImg} alt="Logo"></img>
+            </div>
+            <button className='google-btn'><FcGoogle className="google-icon"/> Continue with Google</button>
             <p>or</p>
             <input type='email' placeholder='E-mail' />
             <input type='password' placeholder='Password'/>
